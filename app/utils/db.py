030105b96d6f0a4fb639081c7ba75b3cf156f050
GlorianY <22544783+GlorianY@users.noreply.github.com>
2022-03-05 21:15:12 +0100

    add pre-commit hooks

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,28 +1,29 @@
 import os
 import pandas as pd
-from psycopg2 import connect
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
-def create_pg_engine() -> Engine: 
+
+def create_pg_engine() -> Engine:
     """Create Postgres engine."""
-    PG_HOST = os.environ['DB_HOST']
-    PG_USER = os.environ['DB_USER']
-    PG_PASSWORD = os.environ['DB_PASSWORD']
-    PG_PORT = os.environ['DB_PORT']
-    PG_DB_NAME = os.environ['DB_NAME']
+    PG_HOST = os.environ["DB_HOST"]
+    PG_USER = os.environ["DB_USER"]
+    PG_PASSWORD = os.environ["DB_PASSWORD"]
+    PG_PORT = os.environ["DB_PORT"]
+    PG_DB_NAME = os.environ["DB_NAME"]
 
     # Create the connection
-    engine = create_engine(f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB_NAME}")
+    engine = create_engine(
+        f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB_NAME}"
+    )
 
     return engine
 
 
-def get_product_by_brand(brand:str) -> pd.DataFrame:
+def get_product_by_brand(brand: str) -> pd.DataFrame:
     """Get all product data which belong to a specified brand."""
     engine = create_pg_engine()
-    
-    df = pd.read_sql_query(f"SELECT * FROM product WHERE brand = '{brand}'",
-                           engine)
+
+    df = pd.read_sql_query(f"SELECT * FROM product WHERE brand = '{brand}'", engine)
 
     return df
